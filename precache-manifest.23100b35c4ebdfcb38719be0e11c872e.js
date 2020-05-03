b3c575bc87fa6309b821eba9908522a86b3246a6
Marcin Kośka <m.koska2@wp.pl>
2020-05-03 15:57:24 +0200

    Updates

---
+++ precache-manifest.23100b35c4ebdfcb38719be0e11c872e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e7ad3241894653c4890e8e60df62c2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0a90e89c152f5512fd51",
+    "url": "/portfolio/static/css/main.9a445550.chunk.css"
+  },
+  {
+    "revision": "3a20950444893a9076ef",
+    "url": "/portfolio/static/js/2.f9b49c13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f9b49c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a90e89c152f5512fd51",
+    "url": "/portfolio/static/js/main.eb29c423.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cbd93389240f4d883a091e10d14aaf93.js"
+  "/portfolio/precache-manifest.23100b35c4ebdfcb38719be0e11c872e.js"
 );
 
 self.addEventListener('message', (event) => {
