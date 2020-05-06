397ff8c2c6d1534cdb5389d504e56d18a11eb1c4
Marcin Kośka <m.koska2@wp.pl>
2020-05-06 21:18:18 +0200

    Updates

---
+++ precache-manifest.911c19b0c0d555573d9783481094dc6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402024aa8fcc199728df431de7dd9072",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba5481696b1b31a64426",
+    "url": "/portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "f179f0b9f4268fd80822",
+    "url": "/portfolio/static/css/main.fb91a8d7.chunk.css"
+  },
+  {
+    "revision": "ba5481696b1b31a64426",
+    "url": "/portfolio/static/js/2.f84a5969.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/portfolio/static/js/2.f84a5969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f179f0b9f4268fd80822",
+    "url": "/portfolio/static/js/main.95188e05.chunk.js"
+  },
+  {
+    "revision": "92d392baeac6228e7d8b",
+    "url": "/portfolio/static/js/runtime-main.3501ff49.js"
+  }
+]);
